d211bde4cd7ddcc4d821f753009f3460bdc43862
Paul <paulfirmin1@gmail.com>
2020-10-25 18:18:41 +0000

    added local storage

--- script/script.js
+++ script/script.js
@@ -2,12 +2,18 @@ const shelf = JSON.parse(localStorage.getItem("shelf")) || [];
 
 const library = document.getElementById('library');
 const addButton = document.getElementById('add-button');
+const closeButton = document.getElementById('close-btn')
 const modal = document.getElementById('formModal');
 const bookForm = document.getElementById('form')
+const delButton = document.getElementsByClassName('del-btn')
 
 addButton.addEventListener('click', () => {
     modal.style.display = 'block';
-})
+});
+
+closeButton.addEventListener('click', () => {
+    modal.style.display = 'none'
+});
 
 function Book() {
     this.tile = ""
@@ -20,7 +26,6 @@ Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages`;
 }
 
-
 function createBook() {
     modal.style.display = 'none'
 
@@ -31,20 +36,51 @@ function createBook() {
     book.pages = bookForm.pages.value;
     book.read = bookForm.read.value;
 
+    shelf.push(book);
+    localStorage.setItem("shelf", JSON.stringify(shelf));
 
-    const cardTemplate = 
-    `<p>${book.title}</p>` +
-    `<p>By ${book.author}</p>` +
-    `<p>${book.pages} pages</p>` +
-    `<p>${book.read}</p>`;
-
-    const bookCard = document.createElement('div');
-
-    bookCard.setAttribute('class', 'book-card')
-    bookCard.innerHTML = cardTemplate
-    library.appendChild(bookCard);
+    displayLibrary()
 }
 
+function displayLibrary() {
+    library.innerHTML = ""
+
+    for (let i = 0; i < shelf.length; i++) {
+
+        let cardTemplate = 
+        `<button class="del-btn" onclick="deleteBook(` + [i] + `)">X</button>` +
+        `<p>${shelf[i].title}</p>` +
+        `<p>By ${shelf[i].author}</p>` +
+        `<p>${shelf[i].pages} pages</p>` +
+        `<div><p>${shelf[i].read} <p><button class="tog-btn" onclick="markRead(` + [i] + `)">(toggle)</button> </p></div></p>`
+    
+        let bookCard = document.createElement('div');
+    
+        bookCard.setAttribute('class', 'book-card')
+        bookCard.innerHTML = cardTemplate
+        library.appendChild(bookCard);
+    }
+}
+
+function deleteBook(index) {
+    shelf.splice(index, 1);
+    localStorage.setItem("shelf", JSON.stringify(shelf));
+    displayLibrary();
+}
+
+function markRead(index) {
+    if (shelf[index].read === "Read") {
+        shelf[index].read = "Not yet read"
+    }
+    else {
+        shelf[index].read = "Read"
+    }
+
+    localStorage.setItem("shelf", JSON.stringify(shelf));
+    displayLibrary()
+}
+
+displayLibrary();
 
 
 
